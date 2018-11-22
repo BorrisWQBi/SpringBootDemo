30ee386d1780523d68c5af9717ee4b7f1b31b886
BORRISBI\Borris <107193219@qq.com>
2018-11-23 02:40:15 +0800

    20181123 first push

---
+++ src/main/java/com/borris/springboot/demo/SpringBootDemo/TestMain.java
@@ -0,0 +1,7 @@
+package com.borris.springboot.demo.SpringBootDemo;
+
+public class TestMain {
+    public static void main(String[] args){
+        
+    }
+}
---
+++ src/main/java/com/borris/springboot/demo/SpringBootDemo/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.borris.springboot.demo.SpringBootDemo.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.transaction.Transactional;
+
+
+@RestController
+@Component("TestController")
+@RequestMapping(value="/test")
+public class TestController {
+
+    @Transactional
+    @RequestMapping(value="/test2/helloWorld2")
+    public String helloWorld(HttpServletRequest req, HttpServletResponse res){
+        String test = req.getParameter("test");
+        return "hello world";
+    }
+}
