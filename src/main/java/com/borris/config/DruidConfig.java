74375c5329cea7cf930c24af2ab3f4339398ee55
bwq <drizzt_bi@hotmail.com>
2018-11-26 12:12:06 +0800

    20181126
    Signed-off-by: bwq <drizzt_bi@hotmail.com>

--- src/main/java/com/borris/config/DruidConfig.java
+++ src/main/java/com/borris/config/DruidConfig.java
@@ -1,6 +1,8 @@
 package com.borris.config;
 
 import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import com.alibaba.druid.support.spring.stat.DruidStatInterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -9,13 +11,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Configuration
 public class DruidConfig {
     private static final Logger logger = LoggerFactory.getLogger(DruidConfig.class);
 
-    @Bean
+    @Bean("druidServlet")
     public ServletRegistrationBean druidServlet(){
         ServletRegistrationBean srb = new ServletRegistrationBean();
         srb.setServlet(new StatViewServlet());
@@ -29,11 +33,25 @@ public class DruidConfig {
         return srb;
     }
 
-    @Bean
+    @Bean("druidFilter")
     public FilterRegistrationBean druidFilter(){
         FilterRegistrationBean frb = new FilterRegistrationBean();
-
+        frb.setFilter(new WebStatFilter());
+        frb.setName("DruidWebStatFilter");
+        Map<String ,String> initPar = new HashMap<String,String>(3);
+        initPar.put("exclusions","*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        initPar.put("sessionStatMaxCount","1000");
+        initPar.put("profileEnable","true");
+        frb.setInitParameters(initPar);
+        List<String> urls = new ArrayList<String>(1);
+        urls.add("/*");
+        frb.setUrlPatterns(urls);
         return frb;
+    }
+
+    @Bean("druidStatInterceptor")
+    public DruidStatInterceptor druidStatInterceptor(){
+        return new DruidStatInterceptor();
     }
 
 }
