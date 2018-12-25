bacb475598ca3978fbec355638f287532c18606b
BORRISBI\Borris <107193219@qq.com>
2018-12-26 02:00:23 +0800

    aop test

--- src/main/java/com/borris/aop/TestAspect.java
+++ src/main/java/com/borris/aop/TestAspect.java
@@ -1,23 +1,28 @@
 package com.borris.aop;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 @Aspect
 @Component
-@Order(2)
+@Order(1)
 public class TestAspect {
 
 
     @Before("PointcutConfig.allControllerMethod()")
-    public void beforeAllPublicMethod() {
+    public void beforeAllPublicMethod(JoinPoint joinPoint) {
         System.out.println(" beforeAllPublicMethod 111");
+    }
+
+    @Around("PointcutConfig.allControllerMethod()")
+    public void aroundAllPublicMethod(ProceedingJoinPoint pjp) throws Throwable {
+        System.out.println(" aroundAllPublicMethod 111");
+        pjp.proceed();
     }
 
 
--- src/main/java/com/borris/aop/TestAspect2.java
+++ src/main/java/com/borris/aop/TestAspect2.java
@@ -1,16 +1,36 @@
 package com.borris.aop;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-@Order(1)
+@Order(2)
 public class TestAspect2 {
     @Before("PointcutConfig.allControllerMethod()")
     public void beforeAllPublicMethod2() {
         System.out.println(" beforeAllPublicMethod 222");
+    }
+    @Around("PointcutConfig.allControllerMethod()")
+    public void aroundAllPublicMethod(ProceedingJoinPoint pjp) throws Throwable {
+        System.out.println(" aroundAllPublicMethod 222");
+        pjp.proceed();
+    }
+
+    @After("PointcutConfig.allControllerMethod()")
+    public void afterAllPublicMethod(JoinPoint jp){
+
+    }
+
+    @AfterReturning("PointcutConfig.allControllerMethod()")
+    public void afterReturnAllPublicMethod(JoinPoint jp){
+
+    }
+    @AfterThrowing("PointcutConfig.allControllerMethod()")
+    public void afterThrowingAllPublicMethod(JoinPoint jp){
+
     }
 }
